c08fc41475a47be1773ea3b9482c51c9905bbcd1
adityanilabh <56129601+adityanilabh@users.noreply.github.com>
2023-12-12 00:15:13 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+/* (1<<30) - 1 :- shifting 1 to 30 place so we get 1<<30 = 1000000000000000000000000000000; 1<<30-1 means 0111111111111111111111111111111 */
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
