e69c8ee05c2ee754f48903fc0785ec58cfe701d8
adityanilabh <nilabhaditya6@gmail.com>
2024-01-04 01:14:59 +0530

    changesmadetoBidirectionalGraph

--- graph/introductionToGraph/bidirectedGraphImpl/bidirectedGraphImpl.java
+++ graph/introductionToGraph/bidirectedGraphImpl/bidirectedGraphImpl.java
@@ -1,71 +1,51 @@
 import java.util.*;
-class Graph{
-    List<List<Integer>>Edge;
+class Graph {
+    List<Set<Integer>> edges;
     final int size;
-    Graph(int size)
-    {
-        Edge= new ArrayList<>(size);
-        this.size=size;
-        for(int index=0;index<size;index++)
-        {
-            Edge.add(new ArrayList<>());
-        }
-    }
-    public void insert(int vertex1, int vertex2)
-    {
-        Integer vertexInt=vertex1,vertexInt2=vertex2;
-        // if vertex is empty
-        if(Edge.get(vertexInt).isEmpty())
-        {
-              Edge.get(vertexInt).add(vertexInt2);
-              Edge.get(vertexInt2).add(vertexInt);
-        }
-        //check if vertex is already present 
-        else
-        {
-            int flag=0;
-            for(Integer j: Edge.get(vertexInt))
-            {
-                Integer data= j;
-                if(data==vertexInt2)
-                {
-                    flag=1;
-                    break;
-                }
-            }
-            if(flag==0)
-            {
-               Edge.get(vertexInt).add(vertexInt2);
-               Edge.get(vertexInt2).add(vertexInt);
-            }
-            
+
+    Graph(int size) {
+        this.size = size;
+        edges = new ArrayList<>(size);
+        for (int index = 0; index < size; index++) {
+            edges.add(new HashSet<>());
         }
     }
 
-    public void print()
-    {
-        for(int index=0;index<size;index++)
-        {
-            if(!Edge.get(index).isEmpty())
-            {
-                System.out.print((index)+" ->");
-                for(int i: Edge.get(index))
-                {
-                    System.out.print(i+" ");
+    public void insert(int sourceVertex, int destinationVertex) {
+        if (isValidVertex(sourceVertex) && isValidVertex(destinationVertex)) {
+            edges.get(sourceVertex).add(destinationVertex);
+            edges.get(destinationVertex).add(sourceVertex);
+        } else {
+            System.out.println("Invalid vertex provided.");
+        }
+    }
+
+    private boolean isValidVertex(int vertex) {
+        return vertex >= 0 && vertex < size;
+    }
+
+    // Other methods for graph operations
+
+    public void print() {
+        for (int index = 0; index < size; index++) {
+            if (!edges.get(index).isEmpty()) {
+                System.out.print((index) + " -> ");
+                for (int i : edges.get(index)) {
+                    System.out.print(i + " ");
                 }
                 System.out.println();
             }
         }
     }
 }
-class bidirectedGraph{
-    public static void main(String[] args)
-    {
-            Graph g= new Graph(7);
-            g.insert(2,3);
-            g.insert(3,4);
-            g.insert(5,6);
-            g.insert(6,5);
-            g.print();
+
+public class BidirectionalGraph {
+    public static void main(String[] args) {
+        Graph g = new Graph(7);
+        g.insert(2, 3);
+        g.insert(3, 4);
+        g.insert(5, 6);
+        g.insert(6, 5);
+        g.print();
     }
 }
