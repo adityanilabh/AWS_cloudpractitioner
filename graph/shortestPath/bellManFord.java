758f05ba35cbb2b7c1dec7f49a49c1636f8e9460
adityanilabh <nilabhaditya6@gmail.com>
2024-01-16 21:39:29 +0530

    pushed the changes

---
+++ graph/shortestPath/bellManFord.java
@@ -0,0 +1,6 @@
+// what we will do is count all the edges. 
+// then count all the vertex as well.
+// run a loop from 1 to no_of_vertex-1. then update the shortest distance.
+// maintain a shortest distance array as well.
+// now after iterating for no_of_vertex-1, do one more iteration to check for negative cycle. 
+// return the shortest path from one node to other nodes.
