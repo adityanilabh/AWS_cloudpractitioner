0280f69d97cebf40a26d02f6aa5aa3baa81bb494
adityanilabh <nilabhaditya6@gmail.com>
2024-01-16 22:09:10 +0530

    commited

--- graph/shortestPath/bellManFord.java
+++ graph/shortestPath/bellManFord.java
@@ -9,11 +9,38 @@
 */
 import java.util.*;
 class bellManFord {
-    public int networkDelayTime(int[][] times, int n, int k) {
+    public int[] networkDelayTime(int[][] times, int n, int k) {
         int[] shortestPath= new int[n+1];
         Arrays.fill(shortestPath,Interger.MAX_VALUE);
         shortestPath[k]=0;
-        
+        // initialise the iteration for vertex 1 to n-1
+        for(int i=1;i<n;i++)
+        {
+            // iterate the edges now: 
+            for(int[] edges: times)
+            {
+                int src= edges[0];
+                int dest= edges[1];
+                int weight= edges[2];
+                if(shortestPath[dest]!=Integer.MAX_VALUE && shortestPath[src]+weight<shortestPath[dest])
+                {
+                    shortestPath[dest]=shortestPath[src]+weight;
+                }
+            }
+        }
+
+        // negative cycle detection
+        for(int[] edge: times)
+        {
+            int src= edges[0];
+            int dest= edges[1];
+            int weight= edges[2];
+            if(shortestPath[dest]!=Integer.MAX_VALUE && shortestPath[src]+weight<shortestPath[dest])
+            {
+                return new int[];
+            }
+        }
+        return shortestPath;
     }
 
     public static void main(String[] args) {
@@ -21,7 +48,7 @@ class bellManFord {
         int[][] times = {{2, 1, 1}, {2, 3, 1}, {3, 4, 1}};
         int n = 4;
         int k = 2;
-        int result = solution.networkDelayTime(times, n, k);
+        int[] result = solution.networkDelayTime(times, n, k);
         System.out.println("Output: " + result);
     }
 }
